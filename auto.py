686a7ac4cc09b6b68f77a5e13d595a3cfb3efb87
Ubuntu <ubuntu@ip-172-31-35-20.eu-central-1.compute.internal>
2021-08-31 21:46:42 +0000

    fifo

--- auto.py
+++ auto.py
@@ -1,4 +1,3 @@
-#!/home/ubuntu/anaconda3/envs/pytorch_p36/bin/python
 import os
 os.chdir(os.path.join(os.path.expanduser('~'), 'nss-aws-tests', 'codev2_4'))
 
@@ -8,18 +7,16 @@ logging.basicConfig(
     filename=os.path.join(os.path.expanduser('~'), 'nss-aws-tests', 'auto.py.log'),
     datefmt='%Y-%m-%d %H:%M:%S',
     format='[%(asctime)s %(levelname).3s] %(message)s')
-logging.info('開始')
 
 from api_lambda_sqs import sqs
 import time
 while True:
     try:
-        command = f'python VHIT_test.py {" ".join(sqs.cases())}'
-        os.system(command)
-        logging.info(command)
-        # print(sqs.cases())
-        # time.sleep(10)
-    except:
+        # command = f'python VHIT_test.py {" ".join(sqs.cases())}'
+        # os.system(command)
+        # logging.info(command)
+        logging.info(sqs.cases())
+        time.sleep(3)
+    except Exception as f_ck:
+        logging.error(f_ck)
         break
-
-logging.info('結束\n')
