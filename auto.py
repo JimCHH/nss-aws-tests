a3c68609d9381cd770e7119b05bb87489cc7ceaa
Ubuntu <ubuntu@ip-172-31-35-20.eu-central-1.compute.internal>
2021-09-02 12:55:24 +0000

    sleep(60) and idle += 1 while idle < 60

--- auto.py
+++ auto.py
@@ -10,13 +10,23 @@ logging.basicConfig(
 
 from api_lambda_sqs import sqs
 import time
-while True:
+idle = 0
+while idle < 60:
     try:
         command = f'python VHIT_test.py {" ".join(sqs.cases())}'
         os.system(command)
         logging.info(command)
+        idle = 0
         # logging.info(sqs.cases())
         # time.sleep(3)
+    except KeyError:
+        logging.info('Messages available: 0')
+        time.sleep(60)
+        idle += 1
     except Exception as f_ck:
         logging.error(f_ck)
-        break
+        time.sleep(60)
+        idle += 1
+
+import requests
+logging.info(requests.get('https://deqg3un8ha.execute-api.eu-central-1.amazonaws.com/stop').text.replace('\n', ' '))
