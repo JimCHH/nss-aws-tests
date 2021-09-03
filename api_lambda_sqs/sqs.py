1342a6081f6b40118c87de0b4794ec06fc9fe123
十百千 <x1001000@users.noreply.github.com>
2021-09-03 12:49:53 +0800

    Update sqs.py

--- api_lambda_sqs/sqs.py
+++ api_lambda_sqs/sqs.py
@@ -4,6 +4,7 @@ def cases():
     sqs = boto3.client('sqs')
     url = 'https://sqs.eu-central-1.amazonaws.com/960602048864/uploading_cases'
     msg = sqs.receive_message(QueueUrl=url)
+    sqs.delete_message(QueueUrl=url, ReceiptHandle=msg['Messages'][0]['ReceiptHandle'])
     payload = eval(msg['Messages'][0]['Body'])
     site = payload['site']
     cases = payload['cases'].split(',')
