b6f899eadd350d70ea89c74d8472634c1ebb9c07
Ubuntu <ubuntu@ip-172-31-35-20.eu-central-1.compute.internal>
2021-09-04 15:30:01 +0000

    sudo umount S3 before s3fs in .bashrc for root to simply source

--- setup.sh
+++ setup.sh
@@ -8,4 +8,5 @@ pip install --upgrade torch
 pip install --upgrade xmltodict
 
 echo source activate pytorch_p36 >> ~/.bashrc
+echo sudo umount S3 >> ~/.bashrc
 echo s3fs neurobit-asg ~/S3 -o passwd_file=~/.aws/credentials-s3fs -o uid=1000 -o gid=1000 >> ~/.bashrc
