57f857fe8a953919ac11aafdb171f6e5a10dc7e0
Ubuntu <ubuntu@ip-172-31-35-20.eu-central-1.compute.internal>
2021-09-11 14:11:48 +0000

    refactor units

---
+++ api_lambda_sqs/lambda_function_StartEC2Instances.py
@@ -0,0 +1,73 @@
+import boto3
+region = 'eu-central-1'
+instances = ['i-03a49fe156346239a', 'i-0fb06f7d468a63de1']
+custom_name = dict(zip(instances, ['Storage Gateway', 'p2.xlarge']))
+ec2_client = boto3.client('ec2', region_name=region)
+sqs_client = boto3.client('sqs', region_name=region)
+url = 'https://sqs.eu-central-1.amazonaws.com/960602048864/uploading_cases'
+
+with open('tokens') as f:
+    channel_access_token, access_token = f.readline().split()
+# LINE Messaging API
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+line_bot_api = LineBotApi(channel_access_token)
+group_id = 'C8e0c9458a13b4dad203fcc224190a6f8'
+group_id = 'U41dda7349d30c51c503127901df2f27a' # uncomment to push to myself
+# LINE Notify API
+import requests
+headers = {'Content-Type': 'application/x-www-form-urlencoded',
+           'Authorization': f'Bearer {access_token}'}
+
+# import urllib.parse
+import time
+
+def lambda_handler(event, context):
+    try:
+        # query = urllib.parse.parse_qs(event['queryStringParameters'])
+        query = event['queryStringParameters'] # ALREADY a <class 'dict'> parsed by Chrome and Python-requests
+        sqs_client.send_message(QueueUrl=url, MessageBody=str(query))
+        site  = query['site']
+        cases = query['cases']
+    except:
+        query = None
+    else:
+        print(query)
+
+    if query:
+        # line_bot_api.push_message(group_id, TextSendMessage(text=f'{site}上傳{cases}'))
+        # payload = {'message': f'{site}上傳{cases}'}
+        # requests.post('https://notify-api.line.me/api/notify', headers=headers, data=payload)
+        result = [f'{site}上傳{cases}']#[str(query)]
+    else:
+        result = []
+
+    for instance in instances:
+        for t in range(14):
+            try:
+                text = f'{custom_name[instance]} 開機'
+                ec2_client.start_instances(InstanceIds=[instance])
+                print(text)
+                # line_bot_api.push_message(group_id, TextSendMessage(text=text))
+                # payload = {'message': text}
+                # requests.post('https://notify-api.line.me/api/notify', headers=headers, data=payload)
+                break
+            except:
+                text += f'失敗！60秒後重試第{t+1}次'
+                print(text)
+                # line_bot_api.push_message(group_id, TextSendMessage(text=text))
+                # payload = {'message': text}
+                # requests.post('https://notify-api.line.me/api/notify', headers=headers, data=payload)
+                time.sleep(60)
+            finally:
+                result.append(text)
+
+    # line_bot_api.push_message(group_id, TextSendMessage(text='\n'.join(result)))
+    payload = {'message': '\n'+'\n'.join(result)}
+    requests.post('https://notify-api.line.me/api/notify', headers=headers, data=payload)
+    response = {}
+    response['statusCode'] = 200
+    response['headers'] = {}
+    response['headers']['Content-Type'] = 'text/plain; charset=UTF-8'
+    response['body'] = '\n'.join(result)
+    return response
---
+++ api_lambda_sqs/sqs_uploading_cases.py
@@ -0,0 +1,25 @@
+import logging, os
+logging.basicConfig(
+    level=logging.INFO, 
+    filename=os.path.join('/home/ubuntu', 'nss-aws-tests', 'auto.py.log'),
+    datefmt='%Y-%m-%d %H:%M:%S',
+    format='[%(asctime)s %(levelname).3s] %(message)s')
+
+import boto3
+def cases():
+    client = boto3.client('sqs')
+    url = 'https://sqs.eu-central-1.amazonaws.com/960602048864/uploading_cases'
+    msg = client.receive_message(QueueUrl=url)
+    logging.info('sqs_uploading_cases.py: client.receive_message')
+    if msg.get('Messages'):
+        client.delete_message(QueueUrl=url, ReceiptHandle=msg['Messages'][0]['ReceiptHandle'])
+        logging.info('sqs_uploading_cases.py: client.delete_message')
+        payload = eval(msg['Messages'][0]['Body'])
+        site = payload['site']
+        cases = payload['cases'].split(',')
+    else:
+        cases = None
+    return cases
+
+if __name__ == '__main__':
+    print(cases())
