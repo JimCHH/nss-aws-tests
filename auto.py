7fd7e3ab54ad32058e05e68f30718049952de561
Ubuntu <ubuntu@ip-172-31-35-20.eu-central-1.compute.internal>
2021-10-09 08:17:34 +0000

    clear temp files in source & idle for 10 min only

--- auto.py
+++ auto.py
@@ -14,19 +14,36 @@ os.chdir(os.path.join('/home/ubuntu', 'nss-aws-tests', 'tests'))
 
 import time
 idle = 0
-while idle < 60:
+while idle < 10:
     cases = sqs_uploaded_cases.cases()
     if cases:
         for case in cases:
-            test4 = f'python VHIT_test.py {case}'
-            logging.info(test4)
+
             try:
                 os.system('sudo umount /home/ubuntu/S3')
                 os.system('s3fs neurobit-asg /home/ubuntu/S3 -o passwd_file=/home/ubuntu/.aws/credentials-s3fs -o uid=1000 -o gid=1000')
-                correct(case)
-                os.system(test4)
+                date_site_patient = case
+                source = f'/home/ubuntu/S3/{date_site_patient.split("_")[1]}/Result/{date_site_patient}'
             except Exception as e:
                 logging.info(e)
+
+            test123 = f'python Test123_mp4_segmentation_extraction_pkl_visualization_pdf.py {case}'
+            logging.info(test123)
+            try:
+                os.system(test123)
+                os.system(f'rm {source}/*.pkl')
+            except Exception as e:
+                logging.info(e)
+
+            test4 = f'python Test4_VHIT_test.py {case}'
+            logging.info(test4)
+            try:
+                correct(case)
+                os.system(test4)
+                os.system(f'rm {source}/*.CSV')
+            except Exception as e:
+                logging.info(e)
+
         idle = 0
     else:
         time.sleep(60)
