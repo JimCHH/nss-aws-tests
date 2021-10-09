96fd142c887a3f4d60684add5437300a70ad107a
十百千 <x1001000@users.noreply.github.com>
2021-10-09 15:39:42 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 sudo apt update
 sudo apt install -y automake autotools-dev fuse g++ git libcurl4-openssl-dev libfuse-dev libssl-dev libxml2-dev make pkg-config
 sudo apt install -y s3fs
+sudo apt install -y chromium-chromedriver --fix-missing
 
 source activate pytorch_p36
 pip install --upgrade pip
 pip install --upgrade torch
 pip install --upgrade xmltodict
+pip install varname
+pip install reportlab
+pip install svglib
+pip install selenium
 
 echo source activate pytorch_p36 >> ~/.bashrc
 echo sudo umount S3 >> ~/.bashrc
