6feaef5e41bf7851ff8ad1c5e41340e79734a2b6
十百千 <x1001000@users.noreply.github.com>
2021-10-17 23:12:28 +0800

    monthly stats of mp4s

--- aws/lambda_function_S3_bucket_objects_stats.py
+++ aws/lambda_function_S3_bucket_objects_stats.py
@@ -6,24 +6,30 @@ s3 = boto3.resource('s3')
 bucket = s3.Bucket('neurobit-asg')
 
 def lambda_handler(event, context):
-    cases, months = [], []
+    mp4s, Mp4s, months = [], {}, []
     for obj in bucket.objects.all():
-        m = re.search('(\d{8}_H\d{2}_NSS\d{5})/$', obj.key)
-        if m and 'H14' not in m.group(1) and 'Reports' not in obj.key:
-            cases.append(m.group(1))
-    cases = sorted(cases)
-    month = [cases[0]]
+        if 'H14' not in obj.key and 'Reports' not in obj.key:
+            m = re.search('\d{8}_\d{6}_H\d{2}_NSS\d{5}_Test\d.mp4', obj.key)
+            if m:
+                mp4s.append(m.group(0))
+            m = re.search('(\d{8}_H\d{2}_NSS\d{5})/$', obj.key)
+            if m:
+                Mp4s[m.group(1)] = []
+    for mp4 in mp4s:
+        Mp4s[mp4[:8]+mp4[15:28]].append(mp4)
+    cases = sorted(Mp4s)
+    month = ['\n'.join(Mp4s[cases[0]]) + '\n']
     for i in range(1, len(cases)):
         if cases[i][4:6] == cases[i-1][4:6]:
-            month.append(cases[i])
+            month.append('\n'.join(Mp4s[cases[i]]) + '\n')
         else:
             month.append(f'{cases[i-1][:6]} 月份共 {len(month)} 收案')
             months.append('\n'.join(month))
-            month = [cases[i]]
+            month = ['\n'.join(Mp4s[cases[i]]) + '\n']
     month.append(f'{cases[i][:6]} 至今共 {len(month)} 收案')
     months.append('\n'.join(month))
 
     return {
         'statusCode': 200,
-        'body': '\n\n'.join(months)#json.dumps(cases)
+        'body': ('\n'+'-'*40+'\n').join(months)#json.dumps('Hello from Lambda!')
     }
